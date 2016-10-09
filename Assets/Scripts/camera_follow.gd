3d60a1bae511a99fed438d3f7d017ca5cf476e28
Chuck Dixon <chvckd@gmail.com>
2016-10-09 12:55:00 +0100

    Refactor camera follow

--- Assets/Scripts/camera_follow.gd
+++ Assets/Scripts/camera_follow.gd
@@ -1,18 +1,17 @@
 
 extends Camera
 
-# Makes the camera follow the player
+const player_path = 'Ball/Player'
 var player
-var player_position
 var camera_offset
 
 func _ready():
 	set_process(true)
-	player = get_parent().get_node("Ball/Player")
-	player_position = player.get_translation()
+	player = get_parent().get_node(player_path)
+	var player_position = player.get_translation()
 	var camera_position = get_translation()
 	camera_offset = camera_position - player_position
 
 func _process(delta):
-	player_position = player.get_translation()
+	var player_position = player.get_translation()
 	set_translation(player_position + camera_offset)
