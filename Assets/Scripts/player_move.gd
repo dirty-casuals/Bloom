af7ecc800de7cfb50a9f5d03a933656849e60689
Chuck Dixon <chvckd@gmail.com>
2016-10-09 15:15:12 +0100

    Update how the ball moves

--- Assets/Scripts/player_move.gd
+++ Assets/Scripts/player_move.gd
@@ -4,6 +4,7 @@ extends RigidBody
 const sideways_speed = 10
 const max_forwards_speed = 30
 const min_forwards_speed = 5
+const score_decrement_per_frame = 1
 
 var sideways_input = 0
 var forwards_speed = 5
@@ -14,7 +15,17 @@ var score = 0
 
 func _ready():
 	set_fixed_process(true)
+	set_process(true)
 	score_label = get_tree().get_current_scene().get_node('UI/Score')
+
+func _process(delta):
+	score = score - score_decrement_per_frame
+	
+	#if the score was under 0 then it's still 0 so don't update it visually
+	if score < 0:
+		score = 0
+	else:
+		score_label.update_score(score)
 
 func _fixed_process(delta):
 	sideways_input = 0
@@ -24,11 +35,11 @@ func _fixed_process(delta):
 	if Input.is_action_pressed('ui_right'):
 		sideways_input = -1
 
-	if Input.is_action_pressed('ui_up'):
-		forwards_speed = forwards_speed + 1
+	#if Input.is_action_pressed('ui_up'):
+		#forwards_speed = forwards_speed + 1
 
-	if Input.is_action_pressed('ui_down'):
-		forwards_speed = forwards_speed - 1
+	#if Input.is_action_pressed('ui_down'):
+		#forwards_speed = forwards_speed - 1
 
 	if forwards_speed > max_forwards_speed:
 		forwards_speed = max_forwards_speed
