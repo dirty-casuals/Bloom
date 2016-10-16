9d1e96d2a303aed723f07c7da40decd27e7cfe96
Chuck Dixon <chvckd@gmail.com>
2016-10-16 17:16:55 +0100

    New build

--- docs/indexfs.js
+++ docs/indexfs.js
@@ -101,7 +101,7 @@ function assert(check, msg) {
         this.requests[this.name] = null;
       },
     };
-      new DataRequest(0, 2079648, 0, 0).open('GET', '/data.pck');
+      new DataRequest(0, 2080831, 0, 0).open('GET', '/data.pck');
     var PACKAGE_PATH;
     if (typeof window === 'object') {
       PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
